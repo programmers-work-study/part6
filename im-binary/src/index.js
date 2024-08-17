3a97e05dffb8f56d5fa52a7b66fcd9308f6aa4ac
pongdang <pongpong0121@kakao.com>
2024-08-17 20:01:29 +0900

    feat: 시간 포맷팅 구현

--- im-binary/src/index.js
+++ im-binary/src/index.js
@@ -10,6 +10,23 @@ const $starStoptButtonLabel = document.getElementById('start-stop-btn-label');
 // const $LapResetButton = document.getElementById('lap-reset-btn');
 const $lapResetButtonLabel = document.getElementById('lap-reset-btn-label');
 
+function formatCentiseconds(centiseconds) {
+    const totalSeconds = Math.floor(centiseconds / 100);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    const remainingCentiseconds = centiseconds % 100;
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    const formattedCentiseconds = String(remainingCentiseconds).padStart(
+        2,
+        '0'
+    );
+
+    return `${formattedMinutes}:${formattedSeconds}.${formattedCentiseconds}`;
+}
+
 const handleStartButtonClick = () => {
     $starStoptButtonLabel.textContent = '중단';
     $startStopButton.classList.remove('bg-green-600');
@@ -19,7 +36,7 @@ const handleStartButtonClick = () => {
     stopwatch.start();
 
     setInterval(() => {
-        $timer.textContent = stopwatch.centisecond;
+        $timer.textContent = formatCentiseconds(stopwatch.centisecond);
     }, 10);
 };
 
