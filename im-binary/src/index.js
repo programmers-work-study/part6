7e4978dc533bb7a5cc1f333f8cddf748e91266fe
Jeon Ijin <pongpong0121@kakao.com>
2024-08-27 14:42:43 +0900

    [part6] 스톱워치 - 전이진 (#4)
    * init
    
    * feat: 시작, 중단 기능
    
    * feat: 시간 포맷팅 구현
    
    * feat: 랩 기능
    
    * feat: 리셋 기능
    
    * refactor
    
    * feat: 키보드 조작 기능
    
    * refactor
    
    * fix: 최신 lap이 맨 위로 오도록 수정
    
    * feat: 최단, 최장 기록 강조 효과

---
+++ im-binary/src/index.js
@@ -0,0 +1,162 @@
+// TODO: 이 곳에 정답 코드를 작성해주세요.
+import Stopwatch from './stopwatch.js';
+
+const 시작 = '시작';
+const 중단 = '중단';
+const 랩 = '랩';
+const 리셋 = '리셋';
+
+const SHORT_CUTS = {
+    L: 'KeyL',
+    S: 'KeyS',
+};
+
+const $timer = document.getElementById('timer');
+const stopwatch = new Stopwatch();
+
+const $startStopButton = document.getElementById('start-stop-btn');
+const $starStoptButtonLabel = document.getElementById('start-stop-btn-label');
+
+const $lapResetButton = document.getElementById('lap-reset-btn');
+const $lapResetButtonLabel = document.getElementById('lap-reset-btn-label');
+
+const $laps = document.getElementById('laps');
+
+const formatCentiseconds = (centiseconds) => {
+    const totalSeconds = Math.floor(centiseconds / 100);
+    const minutes = Math.floor(totalSeconds / 60);
+    const seconds = totalSeconds % 60;
+
+    const remainingCentiseconds = centiseconds % 100;
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(seconds).padStart(2, '0');
+    const formattedCentiseconds = String(remainingCentiseconds).padStart(
+        2,
+        '0'
+    );
+
+    return `${formattedMinutes}:${formattedSeconds}.${formattedCentiseconds}`;
+};
+
+const handleStartButtonClick = () => {
+    stopwatch.start();
+
+    setInterval(() => {
+        $timer.textContent = formatCentiseconds(stopwatch.centisecond);
+    }, 10);
+
+    $starStoptButtonLabel.textContent = '중단';
+    $startStopButton.classList.remove('bg-green-600');
+    $startStopButton.classList.add('bg-red-600');
+    $lapResetButtonLabel.textContent = '랩';
+};
+
+const handleStopButtonClick = () => {
+    stopwatch.pause();
+
+    $starStoptButtonLabel.textContent = '시작';
+    $startStopButton.classList.remove('bg-red-600');
+    $startStopButton.classList.add('bg-green-600');
+    $lapResetButtonLabel.textContent = '리셋';
+};
+
+let $minLap = null;
+let $maxLap = null;
+
+const colorMinMax = () => {
+    $minLap.classList.add('text-green-600');
+    $maxLap.classList.add('text-red-600');
+};
+
+const handleLapButtonClick = () => {
+    const [lapCount, lapTime] = stopwatch.createLap();
+
+    const $lapItem = document.createElement('li');
+
+    $lapItem.classList.add(
+        'flex',
+        'justify-between',
+        'py-2',
+        'px-3',
+        'border-b-2'
+    );
+    $lapItem.innerHTML = `
+            <span>랩 ${lapCount}</span>
+            <span>${formatCentiseconds(lapTime)}</span>
+        `;
+
+    $lapItem.setAttribute('data-time', lapTime);
+
+    $laps.prepend($lapItem);
+
+    if ($minLap == null) {
+        $minLap = $lapItem;
+        return;
+    }
+    if ($maxLap == null) {
+        if (lapTime < $minLap.dataset.time) {
+            $maxLap = $minLap;
+            $minLap = $lapItem;
+        } else {
+            $maxLap = $lapItem;
+        }
+        colorMinMax();
+        return;
+    }
+
+    if (lapTime < $minLap.dataset.time) {
+        $minLap.classList.remove('text-green-600');
+        $minLap = $lapItem;
+    } else if (lapTime > $maxLap.dataset.time) {
+        $maxLap.classList.remove('text-red-600');
+        $maxLap = $lapItem;
+    }
+    colorMinMax();
+};
+
+const handleResetButtonClick = () => {
+    stopwatch.reset();
+
+    $timer.textContent = '00:00.00';
+    $laps.innerHTML = '';
+};
+
+const handleStartStopButtonClick = () => {
+    switch ($starStoptButtonLabel.textContent) {
+        case 시작:
+            handleStartButtonClick();
+            break;
+        case 중단:
+            handleStopButtonClick();
+            break;
+    }
+};
+
+const handleLapResetButtonClick = () => {
+    switch ($lapResetButtonLabel.textContent) {
+        case 랩:
+            handleLapButtonClick();
+            break;
+        case 리셋:
+            handleResetButtonClick();
+            break;
+    }
+};
+
+const handleKeyUp = (event) => {
+    switch (event.code) {
+        case SHORT_CUTS.L:
+            handleLapResetButtonClick();
+            break;
+        case SHORT_CUTS.S:
+            handleStartStopButtonClick();
+            break;
+    }
+};
+
+$startStopButton.addEventListener('click', handleStartStopButtonClick);
+
+$lapResetButton.addEventListener('click', handleLapResetButtonClick);
+
+document.addEventListener('keyup', handleKeyUp);
